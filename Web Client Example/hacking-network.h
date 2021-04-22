a269a804c31e9603d24cabfbced08ebf9dacdd53
cin <lhc22018@gmail.com>
2021-04-22 14:56:19 -0400

    simple web server

---
+++ Web Client Example/hacking-network.h
@@ -0,0 +1,12 @@
+/* This function accepts a socket FD and a ptr to the null terminated
+* string to send. The function will make sure all the bytes of the
+* string are sent. Returns 1 on success and 0 on failure.
+*/
+
+int send_string(int sockfd, unsigned char buffer){
+    int sent_bytes, bytes_to_send;
+    bytes_to_send = strlen(buffer)
+    whiel(bytes_to_send > 0){
+        send_bytes = send 
+    }
+}
